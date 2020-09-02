342d67b15279aa0d57eaad7bfbc26be5759c6bad
kathir <khirshanth123@gmail.com>
2020-09-02 10:56:26 -0400

    firstcommit

---
+++ index.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.typing', {
+    strings: ["STOP USING PAPER", "GO E-SIGNATURE","SAVE TIME AND MONEY"],
+    typeSpeed: 70,
+    backspeed:70,
+    backdelay:1000
+  });
+
+$(".num").counterUp({
+  time:1000,
+  delay:10
+})
+
